6ec76b72ed85883c73d1b83111280884026c65fd
AndrewKing <11490628+andrew000@users.noreply.github.com>
2020-07-02 22:31:58 +0300

    remove A1Z26.py

--- CypherX/__init__.py
+++ CypherX/__init__.py
@@ -1,7 +1,6 @@
 __author__ = "Andrew King"
 
-from .A1Z26 import A1Z26
 from .CaesarCypher import CaesarCypher, CaesarCracker
 from .VigenereCypher import VigenereCypher
 
-__all__ = ['CaesarCypher', 'CaesarCracker', 'VigenereCypher', 'A1Z26']
+__all__ = ['CaesarCypher', 'CaesarCracker', 'VigenereCypher']
